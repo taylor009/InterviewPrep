97cc87fe9e43f91d174b51f69d9352cdb795018f
taylor <taylor@affair.dev>
2020-11-20 03:29:18 -0500

    Completed reverse in parentheses codefights challenge.

---
+++ codesignal/smoothSailing/reverseInParentheses.js
@@ -0,0 +1,14 @@
+function reverseInParentheses(s) {
+    if (s.includes('(')){
+        return reverseInParentheses(reverseOnce(s));
+    } else {
+        return s;
+    }
+}
+
+function reverseOnce(s){
+    var regexp = /\(([^()]*)\)/i;
+    var subStr = regexp.exec(s)[1];
+    subStr = subStr.split('').reverse().join('');
+    return s.replace(regexp, subStr)
+}
